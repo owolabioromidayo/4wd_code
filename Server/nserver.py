be77d003bb6e3bc8ce0f5e863904d51faafe7646
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2021-12-14 12:39:51 +0100

    hosting on local ip & move index.html to templates/

--- Server/nserver.py
+++ Server/nserver.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, render_template
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import cv2, time
+import cv2, time, socket
 
 app = Flask(__name__)
 
@@ -11,6 +11,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    return s.getsockname()[0]
 
 
 def generate_img():
@@ -34,4 +40,4 @@ def get_img():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host=get_local_ip(), port=8000, debug=True)
