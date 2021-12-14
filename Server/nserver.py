bb9c112adc5a275d89787fa75d2ead1c3fffc043
Oromidayo Owolabi <owolabi.oromidayo@stu.cu.edu.ng>
2021-12-14 12:11:13 +0100

    try capturing images instead of using continuous_stream

--- Server/nserver.py
+++ Server/nserver.py
@@ -1,13 +1,13 @@
 from flask import Flask, Response, render_template
-import picamera, io
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2, time
 
 app = Flask(__name__)
 
 
-im_width = 1280 
-im_height = 720
-camera = picamera.PiCamera(resolution=(im_width, im_height), framerate = 60 )
-stream = io.BytesIO()
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
 
 
 @app.route('/')
@@ -15,24 +15,14 @@ def index():
     return render_template('index.html')
 
 
-
-
 def generate_img():
-    for _ in camera.capture_continuous(stream, 'jpeg', use_video_port = True):
-        try:
-            stream.seek(0)
-            image = cv2.imdecode(np.frombuffer(stream.read(), np.uint8), 1)
-
-            stream.seek(0)
-            stream.truncate()
-
-            yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
-        except Exception as e:
-            print("Error: No Image")
-            return
-        
-
-
+    try:
+        image = rawCapture.array
+        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+    except Exception as e:
+        print("Error: No Image")
+        return
+    
 
 @app.route('/get_img')
 def get_img():
