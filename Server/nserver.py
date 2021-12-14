16bd24c4a8e00834992a44f8434f50c6dfbda872
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2021-12-14 14:33:19 +0100

    Video Stream working now

--- Server/nserver.py
+++ Server/nserver.py
@@ -1,43 +1,36 @@
-from flask import Flask, Response, render_template
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import cv2, time, socket
+from flask import Flask, render_template, Response, request
+from camera import VideoCamera
+import time, threading, os, socket
+
+pi_camera = VideoCamera(flip=False)
 
 app = Flask(__name__)
-
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(('8.8.8.8', 80))
     return s.getsockname()[0]
 
+@app.route('/')
+def index():
+    return render_template('index.html') 
 
-def generate_img():
-    try:
-        camera = PiCamera()
-        rawCapture = PiRGBArray(camera)
-        time.sleep(0.1)
-        camera.capture(rawCapture, format="bgr")
-        image = rawCapture.array
-        camera.close()
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
-    except Exception as e:
-        print("Error: No Image")
-        return
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host=get_local_ip(), debug=False)
+
     
 
-@app.route('/get_img')
-def get_img():
-    return Response(generate_img(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 
-if __name__ == "__main__":
-    app.run(host=get_local_ip(), port=8000, debug=True)
